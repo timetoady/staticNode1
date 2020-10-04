31dd90c42663b518300ced49a420ada6e7b9f345
ADAM ANDREASON <adamandreason@gmail.com>
2020-10-03 19:15:17 -0600

    added template site

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.use(express.static('public'))
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port);
